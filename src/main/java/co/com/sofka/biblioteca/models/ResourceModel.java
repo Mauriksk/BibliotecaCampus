3dcf4babbc83c237adbde1820776aebf55441f2d
Mauricio Torres <otarksk@gmail.com>
2022-04-05 00:03:46 -0300

    Trabajo realizado

---
+++ src/main/java/co/com/sofka/biblioteca/models/ResourceModel.java
@@ -0,0 +1,80 @@
+package co.com.sofka.biblioteca.models;
+
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "biblioteca")
+public class ResourceModel {
+
+    @Id
+    private String id = UUID.randomUUID().toString();
+
+
+    private String nombre;
+    private String tema;
+    private String tipo;
+    private Boolean disponible;
+    private String fechaPrestamo;
+
+
+    public ResourceModel(){}
+
+    public ResourceModel(String fechaPrestamo, Boolean disponible, String nombre, String tipo, String tema) {
+        this.fechaPrestamo = fechaPrestamo;
+        this.disponible = disponible;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.tema = tema;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(String fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public Boolean getDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(Boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+}
